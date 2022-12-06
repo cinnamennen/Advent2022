f6b0b3cc909f50b316edb8075350c463d1770741
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2022-12-06 11:53:34 -0500

    day 6

---
+++ src/day06/index.ts
@@ -0,0 +1,77 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+function find_packet(message_size: number, input: string) {
+  const offset = message_size - 1;
+  for (let i = offset; i < input.length; i++) {
+    const str = input.slice(i - offset, i + 1);
+    if (new Set(str).size == message_size) return i + 1;
+  }
+  return -1;
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return find_packet(4, input);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return find_packet(14, input);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 7,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 5,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 6,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 10,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 11,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 19,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 23,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 23,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 29,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 26,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
