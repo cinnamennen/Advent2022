00b2c874eb311a9518afbb3039906a9104183b94
Caleb Mennen <2693113+cinnamennen@users.noreply.github.com>
2022-12-12 09:48:20 -0500

    day 8b

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%86/yellow)](src/day08)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -118,9 +118,9 @@ Both parts: 1.5657079999999999ms
 
 ```
 Day 08
-Time part 1: 215.498ms
-Time part 2: -
-Both parts: 215.497708ms
+Time part 1: 217.815ms
+Time part 2: 182.35ms
+Both parts: 400.16504199999997ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 15/50
-Total time: 229.55ms
+Total stars: 16/50
+Total time: 414.217ms
 ```
 
 <!--/RESULTS-->
--- src/day08/index.ts
+++ src/day08/index.ts
@@ -6,7 +6,7 @@ const parseInput = (rawInput: string) =>
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  let visible = input.length *2 + input[0].length * 2 -4;
+  let visible = input.length * 2 + input[0].length * 2 - 4;
   for (let i = 1; i < input.length - 1; i++) {
     for (let j = 1; j < input[i].length - 1; j++) {
       let seen: number[][] = [];
@@ -16,18 +16,46 @@ const part1 = (rawInput: string) => {
       seen.push(t[j].slice(0, i));
       seen.push(t[j].slice(i + 1));
       seen = seen.map((v) => v.filter((tree) => tree >= input[i][j]));
-      const clean = seen.filter((v) => v.length>0);
+      const clean = seen.filter((v) => v.length > 0);
       const hiding = clean.length;
-      if (hiding <4) visible+=1
+      if (hiding < 4) visible += 1;
     }
   }
   return visible;
 };
 
 const part2 = (rawInput: string) => {
+  let best = 0;
   const input = parseInput(rawInput);
+  for (let i = 1; i < input.length - 1; i++) {
+    for (let j = 1; j < input[i].length - 1; j++) {
+      let seen: number[][] = [];
+      seen.push(input[i].slice(0, j));
+      seen.push(input[i].slice(j + 1));
+      const t = transpose(input) as number[][];
+      seen.push(t[j].slice(0, i));
+      seen.push(t[j].slice(i + 1));
 
-  return;
+      seen[0].reverse();
+      seen[2].reverse();
+
+      const w = seen
+        .map((view) => {
+          let rv = 0;
+          for (const number of view) {
+            rv += 1;
+            if (number >= input[i][j]) break;
+          }
+          return rv;
+        })
+        .reduce(
+          (previousValue, currentValue) => previousValue * currentValue,
+          1,
+        );
+      best = Math.max(best, w)
+    }
+  }
+  return best;
 };
 
 run({
@@ -48,10 +76,16 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        30373
+        25512
+        65332
+        33549
+        35390
+        `,
+        expected: 8,
+      },
     ],
     solution: part2,
   },
